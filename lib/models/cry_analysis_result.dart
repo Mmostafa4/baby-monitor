f9e06b36f1703f9d616c7c53b9e44ac1b61223f7
Mmostafa4 <mostafa@apexadvisor.art>
2026-09-21 14:12:16 +0300

    Update beta cry score and care guidance

--- lib/models/cry_analysis_result.dart
+++ lib/models/cry_analysis_result.dart
@@ -1,12 +1,14 @@
 class CryAnalysisResult {
   final String category;
   final String advice;
+  final int scorePercent;
   final bool urgent;
   final bool experimental;
 
   const CryAnalysisResult({
     required this.category,
     required this.advice,
+    required this.scorePercent,
     required this.urgent,
     required this.experimental,
   });
